d0f2fd22c15ea5a5ffc9d3710ee2cd669330e28c
Justice-Udehegbunam <justicefavour491@gmail.com>
2024-05-07 03:52:41 +0100

    second commit

---
+++ 03-task-manager/Readme.md
@@ -0,0 +1,5 @@
+# Task-Manager Full-stack app
+
+## About
+
+- This contains a full-stack project whereby the frontend has been provided already but i am responsible for creating and connecting the backend to the cloud so that it sunctions every where
--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+  res.send("Task manager App.");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(port, () => {
+  console.log(`App started on port ${port}...`);
+});
