3ee20f9d26b465c12b092a344efaad1779cc1c0b
Justice-Udehegbunam <justicefavour491@gmail.com>
2024-05-17 17:58:52 +0100

    created base app setup

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,32 @@
-console.log('04 Store API')
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// Middlewares
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send(`<h1>Store API</h1> <a href ="/api/v1/products">Products route</a>`);
+});
+
+// Products Route
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const start = async () => {
+  try {
+    // connectDB
+    app.listen(port, console.log(`Server listening on port ${port}....`));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
